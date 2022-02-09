5f9c1630fe62058d0f3267a71418373bb305e82d
lauraPrp <laura.porpiglia@gmail.com>
2022-02-09 23:53:02 +0000

    this commit tests all 15 seats booking FAIL Warning:(28, 9) org.opentest4j.AssertionFailedError: Expected java.lang.UnsupportedOperationException to be thrown, but nothing was thrown.

--- src/test/java/org/laura/cinnamoncinema/CinnamonCinemaTest.java
+++ src/test/java/org/laura/cinnamoncinema/CinnamonCinemaTest.java
@@ -3,19 +3,30 @@ package org.laura.cinnamoncinema;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.FileNotFoundException;
+import java.lang.UnsupportedOperationException;
 
 public class CinnamonCinemaTest {
 
     @Test
-    public void bookOneSeat(){
-        Cinema cinemaCinnamon=new Cinema();
-
-
-       int result = cinemaCinnamon.book(1);
-
-        assertEquals(14,result);
+    public void bookOneSeat() {
+        //arrange
+        Cinema cinemaCinnamon = new Cinema();
+        //act
+        int result = cinemaCinnamon.book(1);
+        //assert
+        assertEquals(14, result);
     }
 
+    @Test
+    public void tryBookingAllSeats() {
+
+        Cinema cinemaCinnamon = new Cinema();
+
+        assertThrows(UnsupportedOperationException.class, () -> cinemaCinnamon.book(15));
+    }
 
 
 }
