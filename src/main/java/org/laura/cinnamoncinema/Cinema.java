377a97581077f51634ef386b8676e0cba97d42c9
lauraPrp <laura.porpiglia@gmail.com>
2022-02-10 16:30:56 +0000

    PASS test assign right single Seat refactored method book and implemented method checkNextAvailableSeat

--- src/main/java/org/laura/cinnamoncinema/Cinema.java
+++ src/main/java/org/laura/cinnamoncinema/Cinema.java
@@ -1,32 +1,43 @@
 package org.laura.cinnamoncinema;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class Cinema {
-    private int maxSeats = 15;
-    private int availableSeats;
-    private int rowNum = 3;
-    private int colNum = 5;
-    private Seat currentSeat;
+
+    private final Dictionary<Integer,Character> cinemaRows;
+    private final int allSeats = 15;
+
+    private final int COL_NUM = 5;
+
     private Seat[][] emptyTheatre;
 
-    public Cinema(int rows, int cols) {
-        Seat[][] seatArray= new Seat[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-            currentSeat =new Seat();
-            // @todo:does it make sense to initialize row and cols for each seat?
-            currentSeat.setBooked(false);
-                seatArray[i][j]=currentSeat;
+    public Cinema() {
+        cinemaRows = new Hashtable<>();
+        cinemaRows.put(0, 'A');
+        cinemaRows.put(1, 'B');
+        cinemaRows.put(2, 'C');
+        //  Seat[][] seatArray = new Seat[cinemaRows.size()][cols];
+        emptyTheatre = new Seat[cinemaRows.size()][COL_NUM];
+        for (int i = 0; i < cinemaRows.size(); i++) {
+            for (int j = 0; j < COL_NUM; j++) {
+                Seat currentSeat = new Seat();
+                currentSeat.setBooked(false);
+                currentSeat.setRow( cinemaRows.get(i));
+                currentSeat.setSeatNumber(j);
+                emptyTheatre[i][j] = currentSeat;
             }
         }
-        this.emptyTheatre=seatArray;
+        //this.emptyTheatre = emptyTheatre;
     }
 
 
-    public int book(int seatToBook) {
-        if (seatToBook > 3)
-            throw new UnsupportedOperationException("You can book max 3 seats");
-
-        return maxSeats - seatToBook;
+    public ArrayList<Seat> book(int seatToBook) {
+        final int MAX_BOOKABLE_SEATS = 3;
+        if (seatToBook > MAX_BOOKABLE_SEATS) throw new UnsupportedOperationException("You can book max 3 seats");
+        //ArrayList<Seat> bookedSeats=  checkNextAvailableSeat();
+        return checkNextAvailableSeat();
 
     }
 
@@ -36,6 +47,26 @@ public class Cinema {
 
     public void setEmptyTheatre(Seat[][] emptyTheatre) {
         this.emptyTheatre = emptyTheatre;
+    }
+
+    private ArrayList<Seat> checkNextAvailableSeat() {
+        ArrayList<Seat> avalableSeats = new ArrayList<>();
+        int availableSeats = 0;
+        boolean freeSeatFound = false;
+        for (int i = 0; i < cinemaRows.size(); i++) {
+            for (int j = 0; j < COL_NUM; j++) {
+
+                if (!emptyTheatre[i][j].isBooked()) {
+                    freeSeatFound = true;
+                    avalableSeats.add(emptyTheatre[i][j]);
+                    break;
+                }
+            }
+            if (freeSeatFound)
+                break;
+        }
+        return avalableSeats;
+
     }
 
 
--- src/test/java/org/laura/cinnamoncinema/CinnamonCinemaTest.java
+++ src/test/java/org/laura/cinnamoncinema/CinnamonCinemaTest.java
@@ -6,55 +6,54 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.lang.UnsupportedOperationException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CinnamonCinemaTest {
     public Cinema cinemaCinnamon;
     public Seat[][] seats;
+
     @BeforeEach
-public void setUpCinemaManager(){
+    public void setUpCinemaManager() {
         //arrange
-        cinemaCinnamon = new Cinema(3,5);
-        seats=cinemaCinnamon.getEmptyTheatre();
-        }
+        cinemaCinnamon = new Cinema();
+        seats = cinemaCinnamon.getEmptyTheatre();
+    }
 
-        @Test
+    @Test
     public void bookOneSeat() {
-
-        //act
-        int result = cinemaCinnamon.book(1);
+        ArrayList<Seat> bookedSeats = cinemaCinnamon.book(1);
         //assert
-        assertEquals(14, result);
+        assertEquals(1, bookedSeats.size());
     }
 
     @Test
     public void tryBookingAllSeats() {
         assertThrows(UnsupportedOperationException.class, () -> cinemaCinnamon.book(15));
     }
-    @Test
-    public void checkAllSeatAreBookable(){
-       //act
 
-        Seat currentSeat = new Seat();
-        for(int i=0;i<3;i++){
-            for(int j=0;j<5;j++){
-               currentSeat= seats[i][j];
-                assertEquals(false,currentSeat.isBooked());
+    @Test
+    public void checkAllSeatAreBookableWhenNoBookingHasBeenMade() {
+        //act
+        Seat currentSeat;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                currentSeat = seats[i][j];
+                assertFalse(currentSeat.isBooked());
             }
         }
 
     }
 
     @Test
-    public void assignCorrectSeatFirstBookingRowANumberOne(){
-       //arrange
-        Seat bookedSeat =new Seat();
-       //act
-        int result = cinemaCinnamon.book(1);
+    public void assignCorrectSeatFirstBookingRowANumberOne() {
+        //arrange
+        ArrayList<Seat> bookedSeats = cinemaCinnamon.book(1);
+
         //assert
-        assertEquals('a',bookedSeat.getRow());
-        assertEquals('1',bookedSeat.getSeatNumber());
+        assertEquals('A', bookedSeats.get(0).getRow());
+        assertEquals(0, bookedSeats.get(0).getSeatNumber());
     }
 
 }
