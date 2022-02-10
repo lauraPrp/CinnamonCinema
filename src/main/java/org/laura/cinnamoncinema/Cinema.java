6c13a8f4be9ac6da461b645626667602ad87baec
lauraPrp <laura.porpiglia@gmail.com>
2022-02-10 16:44:00 +0000

    GREEN/REFACTOR test PASS generate 4>RandomNumber>0

--- src/main/java/org/laura/cinnamoncinema/Cinema.java
+++ src/main/java/org/laura/cinnamoncinema/Cinema.java
@@ -71,7 +71,10 @@ public class Cinema {
 
     public int generateRandomBookingsNumber() {
 
-        return 0;
+        int min = 1;
+        int max = 3;
+
+        return (int)Math.floor(Math.random()*(max-min+1)+min);
     }
 
 
